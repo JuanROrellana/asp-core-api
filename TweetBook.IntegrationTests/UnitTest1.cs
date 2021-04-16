a9cf6e65002808dadf99bcc736fbebb5ae8f1dc8
Juan Ramirez <juanramirez@Juans-MacBook-Pro.local>
2021-04-16 00:31:48 -0600

    Add testing

--- TweetBook.IntegrationTests/UnitTest1.cs
+++ TweetBook.IntegrationTests/UnitTest1.cs
@@ -1,9 +1,11 @@
+using System.Net.Http;
 using NUnit.Framework;
 
 namespace TweetBook.IntegrationTests
 {
     public class Tests
     {
+        private readonly HttpClient _client;
         [SetUp]
         public void Setup()
         {
